6a7c7e64a85ab22c1e8d910c7550ff7ed000a0e5
MaxOnGreeeeen <mas81@tpu.ru>
2022-03-15 21:55:06 +0700

    created

--- client/assets/scripts/index.js
+++
@@ -1,8 +0,0 @@
-import {preventForm} from "./preventForm.js";
-import {copyElement} from "./copyElement.js";
-
-
-window.addEventListener("DOMContentLoaded",() =>{
-    preventForm();
-    copyElement();
-})
